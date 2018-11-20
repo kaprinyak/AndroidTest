9067e9429d00db6b6b12ba48aac079517dbaa409
Kaprinyák Péter <kaprinyak.peter@evolution-consulting.hu>
2018-11-20 11:15:17 +0100

    LoginActivity create

--- app/src/main/java/com/example/kaprinyakpeter/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/kaprinyakpeter/myapplication/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.example.kaprinyakpeter.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, "asd", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
